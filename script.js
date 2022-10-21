4c0a6123fc792e1b0336ab1f3b8293b3ba2f205a
Hendrik Oude Hengel <hendrik.oude@smail.inf.h-brs.de>
2022-10-21 12:56:38 +0200

    Burger div

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 async function changeView(param){
     console.log(param)
 }
+
+async function showNav(){
+    const burger = document.querySelector('.burger');
+    const navList = document.querySelector('.navList');
+    navList.classList.toggle('nav-active');
+    burger.classList.toggle('trans');
+}
