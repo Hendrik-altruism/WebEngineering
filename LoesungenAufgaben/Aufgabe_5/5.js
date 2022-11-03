697e726f861cfde10ef35ad22d2e33a9ddd9041d
Hendrik Oude Hengel <hendrik.oude@smail.inf.h-brs.de>
2022-11-03 09:14:34 +0100

    5 fertig

--- LoesungenAufgaben/Aufgabe_5/5.js
+++ LoesungenAufgaben/Aufgabe_5/5.js
@@ -1,36 +1,38 @@
 var current = 0;
-var Dates = [new Date()];
+var Dates = [];
 var int;
 
 document.getElementById('add').addEventListener("click", async function(){
-    current++;
+    let akt = Dates.length;
     const node = document.createElement("li");
     node.appendChild(document.createTextNode(document.getElementById('name').value));
     const time = document.createElement("time")
-    time.setAttribute("id", "t"+current)
+    time.setAttribute("id", "t"+akt)
     Dates.push(new Date());
-    Dates[current].setTime(-3600000);
-    time.innerHTML= Dates[current].toLocaleTimeString();
+    Dates[akt].setTime(-3600000);
+    time.innerHTML= " "+Dates[akt].toLocaleTimeString()+" ";
     node.appendChild(time)
     const button = document.createElement("button");
-    button.setAttribute("onclick", "toggleTime("+current+")")
-    button.setAttribute("id", "b"+current)
+    button.setAttribute("onclick", "toggleTime("+akt+")")
+    button.setAttribute("id", "b"+akt)
     button.appendChild(document.createTextNode("Start!"))
     node.appendChild(button);
     document.getElementById('talkers').appendChild(node)
     document.getElementById('name').value=""
-    toggleTime(current);
+    toggleTime(akt);
 });
 
 
 function myTimer(){
     let day = (Dates[current].getTime()+1000);
     Dates[current].setTime(day)
-    document.getElementById("t"+current).innerHTML = Dates[current].toLocaleTimeString()
+    document.getElementById("t"+current).innerHTML = " "+Dates[current].toLocaleTimeString()+" ";
 }
 
 async function toggleTime(c){
     if(document.getElementById("b"+c).innerHTML=="Start!"){
+        clearInterval(int)
+        document.getElementById("b"+current).innerHTML = "Start!";
         current = c;
         int = setInterval(myTimer, 1000)
         document.getElementById("b"+c).innerHTML="Stopp!"
@@ -39,6 +41,8 @@ async function toggleTime(c){
         document.getElementById("b"+c).innerHTML="Start!"
     }
 }
+
+
 
 
 const a = document.createElement('a');
