a2a9128968f34f4b9b1f9fc0bd370f82b0b1317a
Hendrik Oude Hengel <hendrik.oude@smail.inf.h-brs.de>
2022-11-24 15:59:23 +0100

    Aufgabe 7

--- LoesungenAufgaben/Aufgabe_7/7.js
+++ LoesungenAufgaben/Aufgabe_7/7.js
@@ -52,8 +52,99 @@ const methodize = (fun) => {
 
 //Twice Funktion
 const twice = (fun) => {
-    return tw = (x) => {
-        this.fun(x, x)
+    return newf = (x) => {
+        return fun(x, x)
     }
 }
-console.log(twice(add))
+let double = twice(add)
+let square = twice(mul)
+console.log(double(3))
+console.log(square(3))
+
+//Composeu Funktion
+const composeu = (ufun1, ufun2) => {
+    return newfu = (val1) => {
+        return ufun2(ufun1(val1))
+    }
+}
+console.log(composeu(double, square)(3))
+
+//Composeb Funktion
+const composeb = (bfun1, bfun2) => {
+    return newfb = (x, y, z) => {
+        return bfun2(z, bfun1(y, x))
+    }
+}
+console.log(composeb(add, mul)(2, 3, 5))
+
+//Once Funktion
+const once = (fun1) => {
+    let done = true
+    return newf1 = (x, y) => {
+        if(done){
+            done = false
+            return fun1(x, y)
+        } else {
+            return null
+        }
+    }
+}
+let add_once = once(add)
+console.log(add_once(1,2))
+console.log(add_once(1,2))
+
+//Fabrikfunktion
+const counterf = (x) => {
+    return{ 
+    inc: () => {
+        return ++x;
+    },
+    dec: () => {
+        return --x;
+    }
+    }
+}
+let counter = counterf(10)
+console.log(counter.inc())
+console.log(counter.dec())
+
+//Revocable Funktion
+const revocable = (fun1) => {
+    let revoked = false
+    return{
+        invoke: (x,...y) => {
+            if(!revoked){
+                return fun1(x,...y);
+            }else{
+                console.log("Funktion already Revoked")
+            }
+        },
+        revoke: () => {
+            revoked = true;
+        }
+    }
+}
+let temp = revocable(add)
+console.log(temp.invoke(7, 2))
+temp.revoke()
+temp.invoke(8)
+
+//Array Wrapper
+const vector = () => {
+    let arr = []
+    return{
+    append: (x) => {
+        arr.push(x)
+    },
+    store: (ind, val) => {
+        arr[ind] = val;
+    },
+    get: (i) => {
+        return arr[i]
+    }
+    }
+}
+let vtest = vector()
+vtest.append(2)
+vtest.store(1, 3)
+console.log(vtest.get(1))
---
+++ LoesungenAufgaben/Aufgabe_7/stopwordsfilter.js
@@ -0,0 +1,31 @@
+const stopwords = ["a","ab","aber","ach","acht","achte","achten","achter","achtes","ag","alle","allein","allem","allen","aller","allerdings","alles","allgemeinen","als","also","am","an","ander","andere","anderem","anderen","anderer","anderes","anderm","andern","anderr","anders","au","auch","auf","aus","ausser","ausserdem","außer","außerdem","b","bald","bei","beide","beiden","beim","beispiel","bekannt","bereits","besonders","besser","besten","bin","bis","bisher","bist","c","d","d.h","da","dabei","dadurch","dafür","dagegen","daher","dahin","dahinter","damals","damit","danach","daneben","dank","dann","daran","darauf","daraus","darf","darfst","darin","darum","darunter","darüber","das","dasein","daselbst","dass","dasselbe","davon","davor","dazu","dazwischen","daß","dein","deine","deinem","deinen","deiner","deines","dem","dementsprechend","demgegenüber","demgemäss","demgemäß","demselben","demzufolge","den","denen","denn","denselben","der","deren","derer","derjenige","derjenigen","dermassen","dermaßen","derselbe","derselben","des","deshalb","desselben","dessen","deswegen","dich","die","diejenige","diejenigen","dies","diese","dieselbe","dieselben","diesem","diesen","dieser","dieses","dir","doch","dort","drei","drin","dritte","dritten","dritter","drittes","du","durch","durchaus","durfte","durften","dürfen","dürft","e","eben","ebenso","ehrlich","ei","ei,","eigen","eigene","eigenen","eigener","eigenes","ein","einander","eine","einem","einen","einer","eines","einig","einige","einigem","einigen","einiger","einiges","einmal","eins","elf","en","ende","endlich","entweder","er","ernst","erst","erste","ersten","erster","erstes","es","etwa","etwas","euch","euer","eure","eurem","euren","eurer","eures","f","folgende","früher","fünf","fünfte","fünften","fünfter","fünftes","für","g","gab","ganz","ganze","ganzen","ganzer","ganzes","gar","gedurft","gegen","gegenüber","gehabt","gehen","geht","gekannt","gekonnt","gemacht","gemocht","gemusst","genug","gerade","gern","gesagt","geschweige","gewesen","gewollt","geworden","gibt","ging","gleich","gott","gross","grosse","grossen","grosser","grosses","groß","große","großen","großer","großes","gut","gute","guter","gutes","h","hab","habe","haben","habt","hast","hat","hatte","hatten","hattest","hattet","heisst","her","heute","hier","hin","hinter","hoch","hätte","hätten","i","ich","ihm","ihn","ihnen","ihr","ihre","ihrem","ihren","ihrer","ihres","im","immer","in","indem","infolgedessen","ins","irgend","ist","j","ja","jahr","jahre","jahren","je","jede","jedem","jeden","jeder","jedermann","jedermanns","jedes","jedoch","jemand","jemandem","jemanden","jene","jenem","jenen","jener","jenes","jetzt","k","kam","kann","kannst","kaum","kein","keine","keinem","keinen","keiner","keines","kleine","kleinen","kleiner","kleines","kommen","kommt","konnte","konnten","kurz","können","könnt","könnte","l","lang","lange","leicht","leide","lieber","los","m","machen","macht","machte","mag","magst","mahn","mal","man","manche","manchem","manchen","mancher","manches","mann","mehr","mein","meine","meinem","meinen","meiner","meines","mensch","menschen","mich","mir","mit","mittel","mochte","mochten","morgen","muss","musst","musste","mussten","muß","mußt","möchte","mögen","möglich","mögt","müssen","müsst","müßt","n","na","nach","nachdem","nahm","natürlich","neben","nein","neue","neuen","neun","neunte","neunten","neunter","neuntes","nicht","nichts","nie","niemand","niemandem","niemanden","noch","nun","nur","o","ob","oben","oder","offen","oft","ohne","ordnung","p","q","r","recht","rechte","rechten","rechter","rechtes","richtig","rund","s","sa","sache","sagt","sagte","sah","satt","schlecht","schluss","schon","sechs","sechste","sechsten","sechster","sechstes","sehr","sei","seid","seien","sein","seine","seinem","seinen","seiner","seines","seit","seitdem","selbst","sich","sie","sieben","siebente","siebenten","siebenter","siebentes","sind","so","solang","solche","solchem","solchen","solcher","solches","soll","sollen","sollst","sollt","sollte","sollten","sondern","sonst","soweit","sowie","später","startseite","statt","steht","suche","t","tag","tage","tagen","tat","teil","tel","tritt","trotzdem","tun","u","uhr","um","und","uns","unse","unsem","unsen","unser","unsere","unserer","unses","unter","v","vergangenen","viel","viele","vielem","vielen","vielleicht","vier","vierte","vierten","vierter","viertes","vom","von","vor","w","wahr","wann","war","waren","warst","wart","warum","was","weg","wegen","weil","weit","weiter","weitere","weiteren","weiteres","welche","welchem","welchen","welcher","welches","wem","wen","wenig","wenige","weniger","weniges","wenigstens","wenn","wer","werde","werden","werdet","weshalb","wessen","wie","wieder","wieso","will","willst","wir","wird","wirklich","wirst","wissen","wo","woher","wohin","wohl","wollen","wollt","wollte","wollten","worden","wurde","wurden","während","währenddem","währenddessen","wäre","würde","würden","x","y","z","z.b","zehn","zehnte","zehnten","zehnter","zehntes","zeit","zu","zuerst","zugleich","zum","zunächst","zur","zurück","zusammen","zwanzig","zwar","zwei","zweite","zweiten","zweiter","zweites","zwischen","zwölf","über","überhaupt","übrigens"];
+const tags = ["<", ">", "/", " ",".", "\n"]
+let raw = document.getElementsByClassName("field--type-text-long")[0].innerHTML
+let rawArray = raw.split(" ");
+function testword(y){
+    let valid = true
+    for(const n in tags){
+        if(y.indexOf(tags[n])!=-1){valid = false;}
+    }
+    return valid
+}
+function testFill(x){
+    return stopwords.indexOf(x)===-1&&testword(x)&&x.length>1
+}
+let f1 = rawArray.filter(x => testFill(x))
+const distinctArr = [...new Set(f1)];
+const count = []
+count.length = distinctArr.length
+count.fill(0)
+f1.map(x => count[distinctArr.indexOf(x)]+=1)
+Array.prototype.max = function() {
+    return Math.max.apply(null, this);
+};
+let erg = []
+erg.push([distinctArr[count.indexOf(count.max())], count.max()])
+count[count.indexOf(count.max())] = 0;
+erg.push([distinctArr[count.indexOf(count.max())], count.max()])
+count[count.indexOf(count.max())] = 0;
+erg.push([distinctArr[count.indexOf(count.max())], count.max()])
+console.log(erg)
+
