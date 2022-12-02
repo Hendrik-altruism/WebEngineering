a71e47fb78fcf88b3478810c8eb086a13d0a31ba
Hendrik Oude Hengel <hendrik.oude@smail.inf.h-brs.de>
2022-12-02 17:01:18 +0100

    Aufgabe 8 fertig

--- LoesungenAufgaben/Aufgabe_8/8_3.js
+++ LoesungenAufgaben/Aufgabe_8/8_3.js
@@ -1,6 +1,36 @@
-import {webData} from "./8_3_fun.js";
 
-const data = await webData()
+const webData = async () => {
+    return await (await fetch("8_3.json")).json()
+}
+
+let field = 'home'
+
+async function changeField(f) {
+    field = f
+}
+
+const changeView = async (name) => {
+    const data = await webData()
+    const shownData = data[field][name]
+    const elementLeft = document.getElementById('left')
+    const main = document.querySelector('main')
+    const elementRight = document.getElementById('right')
+
+    elementLeft.innerHTML = `<h1>${name.toUpperCase()}:</h1>`
+    if(name ==='home'){
+        main.innerHTML = '<h3>Home Screen<h3>'
+        elementRight.innerHTML = ''
+    }else{
+        main.innerHTML = `<p>${shownData.content}</p>`
+        elementRight.innerHTML = `${shownData.references}`
+    }
+}
+
+changeField('javascript')
+changeView('function')
+
+
+
 
 
 
--- LoesungenAufgaben/Aufgabe_8/8_3_fun.js
+++
@@ -1,5 +0,0 @@
-"use-strict"
-
-export const webData = async () => {
-    return await (await fetch("8_3.json")).json()
-}
