4a27d7d5ae756caeb795f0de472487b2f1915b67
Hendrik Oude Hengel <hendrik.oude@smail.inf.h-brs.de>
2022-12-23 11:16:22 +0100

    Custom SVGS!!!

---
+++ ProjektWebseite/config.js
@@ -0,0 +1,73 @@
+/* Modul mit den Daten
+   @return {}
+*/
+
+export const webData = {
+    elemente: [
+        {
+            titel: "HTML",
+            img: "./ressources/svg/code-slash.svg",
+            text: "Geschichte des WWW, Einführung in HTTP und HTML mit einer Anwendung"
+        },
+        {
+            titel: "CSS",
+            img: "./ressources/svg/eyedropper.svg",
+            text: "Selektoren und Farben, sowie Positionierung und eine einfache Anwendung"
+        },
+        {
+            titel: "Responsive Web Design",
+            img: "./ressources/svg/phone.svg",
+            text: "Grid, Flexbox und Media Queries"
+        },
+        {
+            titel: "JavaScript",
+            img: "./ressources/svg/braces.svg",
+            text: "Funktionen, Objekte mit den Anwendungen Fibonacci und TopSort"
+        },
+        {
+            titel: "Document Object Model",
+            img: "./ressources/svg/columns-gap.svg",
+            text: "DOM-Operationen, Performanz-Messung sowie RednerListe und Top-Sort als Web-App"
+        },
+        {
+            titel: "ECMAScript",
+            img: "./ressources/svg/equal-chevron.svg",
+            text: "Iteratoren, Generatoren und Proxy für den TopSort-Algorithmus. Aufgabe zu den Klammerpaaren"
+        },
+        {
+            titel: "Funktionale Programmierung",
+            img: "./ressources/svg/speedometer2.svg",
+            text: "Diverse JavaScript Funktionen mit TextAnalyse, basierend auf filter-map-reduce"
+        },
+        {
+            titel: "Asynchrones JavaScript",
+            img: "./ressources/svg/pause-circle.svg",
+            text: "Promises und Async / Await zur Realisierung eines WWW-Navigators mit der Fetch-API"
+        },
+        {
+            titel: "Scalable Vector Graphics",
+            img: "./ressources/svg/filetype-svg.svg",
+            text: "Tic-Tac-Toe durch SVGs visualisiert und Einstiegsaufgabe"
+        },
+        {
+            titel: "TypeScript",
+            img: "./ressources/svg/cup-hot.svg",
+            text: "Fehlersuche und Beispielprogramm in TypeScript zur Visualisierung der Covid-Fallzahlen"
+        },
+        {
+            titel: "Vue 2",
+            img: "./ressources/svg/svg-path.svg",
+            text: "Das erste Kapitel befasst sich mit Html "
+        },
+        {
+            titel: "Server-side Scripting",
+            img: "./ressources/svg/router.svg",
+            text: "Das erste Kapitel befasst sich mit Html "
+        },
+        {
+            titel: "Web App Security",
+            img: "./ressources/svg/shield-lock.svg",
+            text: "Das erste Kapitel befasst sich mit Html "
+        }
+    ]
+}
--- ProjektWebseite/script.js
+++ ProjektWebseite/script.js
@@ -1,3 +1,4 @@
+import {webData} from './config.js';
 
 const listeners = document.querySelectorAll('li').forEach((listener, index) =>{
     listener.addEventListener("click", ()=>{
@@ -20,85 +21,12 @@ async function changeView(value){
 }
 
 
-
-
-
-const topicsData = {
-    elemente: [
-        {
-            titel: "HTML",
-            img: "./ressources/svg/code-slash.svg",
-            text: "Geschichte des WWW, Einführung in HTTP und HTML mit einer Anwendung"
-        },
-        {
-            titel: "CSS",
-            img: "./ressources/svg/eyedropper.svg",
-            text: "Selektoren und Farben, sowie Positionierung und eine einfache Anwendung"
-        },
-        {
-            titel: "Responsive Web Design",
-            img: "./ressources/svg/phone.svg",
-            text: "Grid, Flexbox und Media Queries"
-        },
-        {
-            titel: "JavaScript",
-            img: "./ressources/svg/braces.svg",
-            text: "Funktionen, Objekte mit den Anwendungen Fibonacci und TopSort"
-        },
-        {
-            titel: "Document Object Model",
-            img: "./ressources/svg/columns-gap.svg",
-            text: "Das erste Kapitel befasst sich mit Html "
-        },
-        {
-            titel: "ECMAScript",
-            img: "./ressources/svg/chevron-right.svg",
-            text: "Das erste Kapitel befasst sich mit Html "
-        },
-        {
-            titel: "Funktionale Programmierung",
-            img: "./ressources/svg/speedometer2.svg",
-            text: "Das erste Kapitel befasst sich mit Html "
-        },
-        {
-            titel: "Asynchrones JavaScript",
-            img: "./ressources/svg/pause-circle.svg",
-            text: "Das erste Kapitel befasst sich mit Html "
-        },
-        {
-            titel: "Scalable Vector Graphics",
-            img: "./ressources/svg/filetype-svg.svg",
-            text: "Das erste Kapitel befasst sich mit Html "
-        },
-        {
-            titel: "TypeScript",
-            img: "./ressources/svg/cup-hot.svg",
-            text: "Das erste Kapitel befasst sich mit Html "
-        },
-        {
-            titel: "Vue 2",
-            img: "./ressources/svg/chevron-down.svg",
-            text: "Das erste Kapitel befasst sich mit Html "
-        },
-        {
-            titel: "Server-side Scripting",
-            img: "./ressources/svg/router.svg",
-            text: "Das erste Kapitel befasst sich mit Html "
-        },
-        {
-            titel: "Web App Security",
-            img: "./ressources/svg/shield-lock.svg",
-            text: "Das erste Kapitel befasst sich mit Html "
-        }
-    ]
-}
-
 renderApp();
 
     function renderApp() {
       const $items = document.querySelector( '#cards' );
       $items.innerHTML = '';
-      topicsData.elemente.forEach( element => {
+      webData.elemente.forEach( element => {
         const $item = document.createElement( 'div' );
         const elementSVG = element.img;
         $item.classList.add( 'col' );
@@ -131,4 +59,9 @@ renderApp();
         {const $item = document.createElement( 'div' );
         $item.classList.add( 'col' );
         $items.appendChild( $item );}
+    }
+
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
